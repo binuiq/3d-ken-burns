9c36abf9b6b364f9679d754a0a47e691702eeb36
Bin <hi.binjiang@gmail.com>
2023-07-25 23:00:40 -0700

    add seconds param to autozoom.py

--- autozoom.py
+++ autozoom.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 
-import base64
+# import base64
 import cupy
 import cv2
-import flask
+# import flask
 import getopt
-import gevent
-import gevent.pywsgi
-import glob
-import h5py
-import io
+# import gevent
+# import gevent.pywsgi
+# import glob
+# import h5py
+# import io
 import math
 import moviepy
 import moviepy.editor
 import numpy
 import os
-import random
+# import random
 import re
-import scipy
-import scipy.io
-import shutil
+# import scipy
+# import scipy.io
+# import shutil
 import sys
-import tempfile
-import time
+# import tempfile
+# import time
 import torch
 import torchvision
-import urllib
-import zipfile
+# import urllib
+# import zipfile
 
 ##########################################################
 
@@ -51,46 +51,52 @@ arguments_strIn = './images/doublestrike.jpg'
 arguments_strOut = './autozoom.mp4'
 
 for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ strParameter[2:] + '=' for strParameter in sys.argv[1::2] ])[0]:
-	if strOption == '--in' and strArgument != '': arguments_strIn = strArgument # path to the input image
-	if strOption == '--out' and strArgument != '': arguments_strOut = strArgument # path to where the output should be stored
+    if strOption == '--in' and strArgument != '': arguments_strIn = strArgument # path to the input image
+    if strOption == '--out' and strArgument != '': arguments_strOut = strArgument # path to where the output should be stored
+    if strOption == '--seconds' and strArgument != '': arguments_intSeconds = int(strArgument) # path to where the output should be stored
 # end
 
 ##########################################################
 
 if __name__ == '__main__':
-	npyImage = cv2.imread(filename=arguments_strIn, flags=cv2.IMREAD_COLOR)
+    npyImage = cv2.imread(filename=arguments_strIn, flags=cv2.IMREAD_COLOR)
 
-	intWidth = npyImage.shape[1]
-	intHeight = npyImage.shape[0]
+    intWidth = npyImage.shape[1]
+    intHeight = npyImage.shape[0]
 
-	fltRatio = float(intWidth) / float(intHeight)
+    fltRatio = float(intWidth) / float(intHeight)
 
-	intWidth = min(int(1024 * fltRatio), 1024)
-	intHeight = min(int(1024 / fltRatio), 1024)
+    intWidth = min(int(1024 * fltRatio), 1024)
+    intHeight = min(int(1024 / fltRatio), 1024)
 
-	npyImage = cv2.resize(src=npyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
+    npyImage = cv2.resize(src=npyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
 
-	process_load(npyImage, {})
+    process_load(npyImage, {})
 
-	objFrom = {
-		'fltCenterU': intWidth / 2.0,
-		'fltCenterV': intHeight / 2.0,
-		'intCropWidth': int(math.floor(0.97 * intWidth)),
-		'intCropHeight': int(math.floor(0.97 * intHeight))
-	}
+    objFrom = {
+        'fltCenterU': intWidth / 2.0,
+        'fltCenterV': intHeight / 2.0,
+        'intCropWidth': int(math.floor(0.97 * intWidth)),
+        'intCropHeight': int(math.floor(0.97 * intHeight))
+    }
 
-	objTo = process_autozoom({
-		'fltShift': 100.0,
-		'fltZoom': 1.25,
-		'objFrom': objFrom
-	})
+    objTo = process_autozoom({
+        'fltShift': 100.0,
+        'fltZoom': 1.25,
+        'objFrom': objFrom
+    })
 
-	npyResult = process_kenburns({
-		'fltSteps': numpy.linspace(0.0, 1.0, 75).tolist(),
-		'objFrom': objFrom,
-		'objTo': objTo,
-		'boolInpaint': True
-	})
+    fps = 30
+    num_frames = arguments_intSeconds * fps
+    npyResult = process_kenburns({
+        'fltSteps': numpy.linspace(0.0, 1.0, num_frames).tolist(),
+        'objFrom': objFrom,
+        'objTo': objTo,
+        'boolInpaint': True
+    })
 
-	moviepy.editor.ImageSequenceClip(sequence=[ npyFrame[:, :, ::-1] for npyFrame in npyResult + list(reversed(npyResult))[1:-1] ], fps=25).write_videofile(arguments_strOut)
+    # moviepy.editor.ImageSequenceClip(sequence=[ npyFrame[:, :, ::-1] for npyFrame in npyResult + list(reversed(npyResult))[1:-1] ], fps=25).write_videofile(arguments_strOut)
+    moviepy.editor.ImageSequenceClip(
+            sequence=[npyFrame[:, :, ::-1] for npyFrame in list(reversed(npyResult))],
+            fps=fps).write_videofile(arguments_strOut)
 # end
